60b0d895894d4511271555172ed1c63396153922
erik-grahn <egrahnmusic@gmail.com>
2026-05-04 12:08:37 +0200

    chore (configs): update configs to handle deploymentissue on github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/fed25d-grafiska-verktyg-brand-identity/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 });
