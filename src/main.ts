5aa12882e5dbfc3c50d0c36ae275221d8e46051c
isabelletherese <isabellereynolds@hotmail.com>
2026-05-07 08:57:45 +0200

    feat: add cookie banner with accept and decline functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import './styles/style.scss';
+
+const cookieBanner = document.querySelector('.cookies');
+
+document.getElementById('accept-btn')?.addEventListener('click', () => 
+    cookieBanner?.classList.add('hidden'));
+document.getElementById('decline-btn')?.addEventListener('click', () => 
+    cookieBanner?.classList.add('hidden'));
